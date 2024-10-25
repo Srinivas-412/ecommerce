4013bcdba0442757504c2a4b0a15a8269915b152
Srinivas-412 <kannysrinivas@gmail.com>
2024-10-25 21:31:55 +0530

    ecommerce website

---
+++ routes/productRoutes.js
@@ -0,0 +1,68 @@
+import express from "express";
+import {
+  brainTreePaymentController,
+  braintreeTokenController,
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productCategoryController,
+  productCountController,
+  productFiltersController,
+  productListController,
+  productPhotoController,
+  relatedProductController,
+  searchProductController,
+  updateProductController,
+} from "../controllers/productController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import formidable from "express-formidable";
+
+const router = express.Router();
+
+//routes
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
+//routes
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+
+//get products
+router.get("/get-product", getProductController);
+
+//single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete rproduct
+router.delete("/delete-product/:pid", deleteProductController);
+
+router.post('/product-filters',productFiltersController)
+
+router.get('/product-count',productCountController)
+
+router.get('/product-list/:page',productListController)
+
+router.post('/search/:keyword',searchProductController)
+
+router.get("/related-product/:pid/:cid", relatedProductController);
+
+
+router.get('/braintree/token',braintreeTokenController)
+
+router.post('/braintree/payment',requireSignIn,brainTreePaymentController)
+router.get('product-category/:slug',productCategoryController)
+export default router;
+
