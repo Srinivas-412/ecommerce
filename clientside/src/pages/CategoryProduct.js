7acb65f6bc399e1e1b50c6f3862860d9e171b9d7
Srinivas <162046675+Srinivas-412@users.noreply.github.com>
2025-05-26 11:23:06 +0530

    Update CategoryProduct.js

--- clientside/src/pages/CategoryProduct.js
+++ clientside/src/pages/CategoryProduct.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Layout from "../components/Layout/Layout/Layout";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
+axios.defaults.baseURL = "https://graymart.com";
+
 const CategoryProduct = () => {
   const params = useParams();
   const navigate = useNavigate();
