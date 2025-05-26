756e04f8c239f6c9cebc455748442cd4307e6a27
Srinivas <162046675+Srinivas-412@users.noreply.github.com>
2025-05-26 11:47:37 +0530

    Update ProductDetails.js

--- clientside/src/pages/ProductDetails.js
+++ clientside/src/pages/ProductDetails.js
@@ -4,7 +4,6 @@ import Layout from "../components/Layout/Layout/Layout";
 import { useCart } from "../context/cart";
 import toast from "react-hot-toast";
 import { useParams, useNavigate } from "react-router-dom";
-import React, { useState, useEffect } from "react";
 import Layout from "../components/Layout/Layout/Layout";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
